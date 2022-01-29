5f31bdd1a4107219d79ebffbd8ce8e51af76a999
demo <NamelessAlien1@gmail.com>
2022-01-29 19:16:43 +0400

    rest_framework_simplejwt.token_blacklist

--- backend/django_codenames/settings.py
+++ backend/django_codenames/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # packages
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'debug_toolbar'
 ]
