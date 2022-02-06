01d2f481f5b916d9bf17acab381082067e7d8555
Str4nge1 <aleqsandre.b21@gmail.com>
2022-02-06 22:54:58 +0400

    finalized game page

--- node_frontend/scripts/game.js
+++ node_frontend/scripts/game.js
@@ -14,6 +14,16 @@ function getData(pkArg) {
   };
 }
 
+function notifyUsers(pkArg) {
+  socket.send(
+    JSON.stringify({
+      action: "notify_users",
+      request_id: new Date().getTime(),
+      pk: pkArg,
+    })
+  );
+}
+
 function becomeSpyMaster(pkArg) {
   const [redTeamSpy, blueTeamSpy] = document.querySelectorAll(".spyBtn");
   redTeamSpy.addEventListener("click", function () {
@@ -25,6 +35,7 @@ function becomeSpyMaster(pkArg) {
         team: 0,
       })
     );
+    notifyUsers(pkArg);
   });
 
   blueTeamSpy.addEventListener("click", function () {
@@ -36,6 +47,7 @@ function becomeSpyMaster(pkArg) {
         team: 1,
       })
     );
+    notifyUsers(pkArg);
   });
 }
 
@@ -50,6 +62,7 @@ function becomeOperative(pkArg) {
         team: 0,
       })
     );
+    notifyUsers(pkArg);
   });
 
   blueTeamCop.addEventListener("click", function () {
@@ -61,6 +74,7 @@ function becomeOperative(pkArg) {
         team: 1,
       })
     );
+    notifyUsers(pkArg);
   });
 }
 
@@ -78,122 +92,184 @@ function retrieveAction(pkArg) {
 function socketManager(pkArg) {
   socket.onmessage = function (event) {
     const response = JSON.parse(event.data);
-    console.log(response)
-    const spymaster1 = document.querySelector(".spymaster1");
-    const spymaster2 = document.querySelector(".spymaster2");
-    const [operativeLabel1, operativeLabel2] =
-      document.querySelectorAll(".operatives");
-    const startBtn = document.querySelector(".startGameBtn");
+    console.log(response);
+    console.log(response.data);
+    console.log(response.action);
+    if (response.action === "notify_users") {
+      retrieveAction(pkArg);
+    } else if (response.action === "retrieve") {
+      const spymaster1 = document.querySelector(".spymaster1");
+      const spymaster2 = document.querySelector(".spymaster2");
+      const [operativeLabel1, operativeLabel2] =
+        document.querySelectorAll(".operatives");
+      const startBtn = document.querySelector(".startGameBtn");
 
-    removeChilds(operativeLabel1);
-    removeChilds(operativeLabel2);
+      removeChilds(operativeLabel1);
+      removeChilds(operativeLabel2);
 
-    spymaster1.textContent = "-";
-    spymaster2.textContent = "-";
+      spymaster1.textContent = "-";
+      spymaster2.textContent = "-";
 
-    for (const spy of response.data.spymasters) {
-      spy.team === 0
-        ? (spymaster1.textContent = spy.player)
-        : (spymaster2.textContent = spy.player);
-    }
-
-    for (const op of response.data.field_operatives) {
-      const operative = document.createElement("p");
-      operative.textContent = op.player;
-      operative.id = op.id;
-      if (op.team === 0) {
-        operative.className = "operatives1";
-        operativeLabel1.appendChild(operative);
-      } else if (op.team === 1) {
-        operative.className = "operatives2";
-        operativeLabel2.appendChild(operative);
-      }
-    }
-
-    if (response.data.status >= 1) {
-      const cards = document.querySelectorAll(".card");
-      const cardData = response.data.game_cards;
-      const endBtn = document.querySelector(".endTurnBtn");
-
-      startBtn.classList.add("hidden");
-      for (const btn of [
-        ...document.querySelectorAll(".spyBtn"),
-        ...document.querySelectorAll(".opBtn"),
-      ]) {
-        btn.disabled = true;
+      for (const spy of response.data.spymasters) {
+        spy.team === 0
+          ? (spymaster1.textContent = spy.player)
+          : (spymaster2.textContent = spy.player);
       }
 
-      for (let i = 0; i < cards.length; ++i) {
-        cards[i].textContent = cardData[i].name;
-        if (cardData[i].team === 0) {
-          cards[i].classList.add("red");
-        } else if (cardData[i].team === 1) {
-          cards[i].classList.add("blue");
-        }
-        if (cardData[i].is_assassin) {
-          cards[i].classList.add("killer");
-        }
-        cards[i].id = cardData[i].id;
-        if (cardData[i].is_guessed) {
-          cards[i].classList.add("bright");
+      for (const op of response.data.field_operatives) {
+        const operative = document.createElement("p");
+        operative.textContent = op.player;
+        operative.id = op.id;
+        if (op.team === 0) {
+          operative.className = "operatives1";
+          operativeLabel1.appendChild(operative);
+        } else if (op.team === 1) {
+          operative.className = "operatives2";
+          operativeLabel2.appendChild(operative);
         }
       }
 
-      for (const card of cards) {
-        card.addEventListener("click", function () {
-          socket.send(
-            JSON.stringify({
-              action: "play",
-              request_id: new Date().getTime(),
-              pk: pkArg,
-              game_card_pk: card.id,
-            })
-          );
-        });
-      }
-      if (response.data.status === 1) {
-        endBtn.classList.remove("hidden");
+      if (response.data.status >= 1) {
+        const cards = document.querySelectorAll(".card");
+        const cardData = response.data.game_cards;
+        const endBtn = document.querySelector(".endTurnBtn");
+        const scoreRed = document.querySelector(".lefToGuessRed");
+        const scoreBlue = document.querySelector(".lefToGuessBlue");
 
-        endBtn.addEventListener("click", function () {
-          socket.send(
-            JSON.stringify({
-              action: "end_turn",
-              request_id: new Date().getTime(),
-              pk: pkArg,
-            })
-          );
-        });
+        scoreRed.textContent = response.data.left_red_card_count;
+        scoreBlue.textContent = response.data.left_blue_card_count;
 
-        if (!response.data.can_play) {
-          console.log(2);
+        startBtn.classList.add("none");
+        for (const btn of [
+          ...document.querySelectorAll(".spyBtn"),
+          ...document.querySelectorAll(".opBtn"),
+        ]) {
+          btn.disabled = true;
+        }
+
+        for (let i = 0; i < cards.length; ++i) {
+          cards[i].textContent = cardData[i].name;
+          if (cardData[i].team === 0) {
+            cards[i].classList.add("red");
+          } else if (cardData[i].team === 1) {
+            cards[i].classList.add("blue");
+          }
+          if (cardData[i].is_assassin) {
+            cards[i].classList.add("killer");
+          }
+          cards[i].id = cardData[i].id;
+          if (cardData[i].is_guessed) {
+            cards[i].classList.add("bright");
+            cards[i].classList.add("disabled");
+          }
+        }
+
+        for (const card of cards) {
+          card.addEventListener("click", function () {
+            socket.send(
+              JSON.stringify({
+                action: "play",
+                request_id: new Date().getTime(),
+                pk: pkArg,
+                game_card_pk: card.id,
+              })
+            );
+            notifyUsers(pkArg);
+          });
+        }
+        if (response.data.status === 1) {
+          endBtn.classList.remove("hidden");
+          const [info1, info2] = document.querySelectorAll(".info");
+          const spyOpBtns = [
+            ...document.querySelectorAll(".spyBtn"),
+            ...document.querySelectorAll(".opBtn"),
+          ];
+
+          for (const btn of spyOpBtns) {
+            btn.classList.add("none");
+          }
+          if (response.data.last_turn === 1) {
+            console.log("s");
+            info2.classList.add("brightDiv");
+            info1.classList.remove("brightDiv");
+          } else {
+            info2.classList.remove("brightDiv");
+            info1.classList.add("brightDiv");
+          }
+          endBtn.addEventListener("click", function () {
+            socket.send(
+              JSON.stringify({
+                action: "end_turn",
+                request_id: new Date().getTime(),
+                pk: pkArg,
+              })
+            );
+            notifyUsers(pkArg);
+          });
+
+          if (!response.data.can_play) {
+            for (let i = 0; i < cards.length; ++i) {
+              if (!cardData[i].is_guessed) {
+                cards[i].classList.add("disabled");
+              }
+            }
+            endBtn.classList.add("disabled");
+          } else {
+            for (let i = 0; i < cards.length; ++i) {
+              if (!cardData[i].is_guessed) {
+                cards[i].classList.remove("disabled");
+              }
+            }
+            endBtn.classList.remove("disabled");
+          }
+        } else if (response.data.status === 2) {
+          const spyOpBtns = [
+            ...document.querySelectorAll(".spyBtn"),
+            ...document.querySelectorAll(".opBtn"),
+          ];
+          const parent = document.querySelector(".header");
+          const winner = document.createElement("p");
+          winner.classList.add("winner");
+          endBtn.classList.add("hidden");
           for (const card of cards) {
             card.classList.add("disabled");
           }
-          endBtn.classList.add("disabled");
-        } else {
-          for (const card of cards) {
-            card.classList.remove("disabled");
+
+          for (const btn of spyOpBtns) {
+            btn.classList.add("none");
           }
-          endBtn.classList.remove("disabled");
+
+          if (response.data.last_turn === 0) {
+            winner.textContent = "Red Team Won!";
+          } else {
+            winner.textContent = "Blue Team Won!";
+          }
+
+          const winnerSelected = document.querySelectorAll(".winner");
+
+          if (winnerSelected.length === 1) {
+            console.log("delete");
+            parent.removeChild(winnerSelected[0]);
+          }
+
+          console.log("add");
+          document.querySelector(".header").appendChild(winner);
         }
-      } else if (response.status === 2) {
-        endBtn.classList.add("hidden");
-        for (const card of cards) {
-          card.classList.add("disabled");
+      } else {
+        if (response.data.is_creator) {
+          startBtn.classList.remove("none");
+          startBtn.classList.remove("hidden");
+          startBtn.addEventListener("click", function () {
+            socket.send(
+              JSON.stringify({
+                action: "start_game",
+                request_id: new Date().getTime(),
+                pk: pkArg,
+              })
+            );
+            notifyUsers(pkArg);
+          });
         }
-      }
-    } else {
-      if (response.data.is_creator) {
-        startBtn.classList.remove("hidden");
-        startBtn.addEventListener("click", function () {
-          socket.send(
-            JSON.stringify({
-              action: "start_game",
-              request_id: new Date().getTime(),
-              pk: pkArg,
-            })
-          );
-        });
       }
     }
   };
@@ -217,7 +293,7 @@ function socketManager(pkArg) {
 
 function main() {
   const pk = window.location.href.split("=")[1];
-  getData(pk)
+  getData(pk);
   becomeOperative(pk);
   becomeSpyMaster(pk);
   socketManager(pk);
